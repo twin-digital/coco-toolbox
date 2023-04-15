d84f8df47ad7a680e77433582ab9eee59ebd337a
coco-b0t <92187713+coco-b0t@users.noreply.github.com>
2023-04-15 16:09:45 -0500

    Add missing import statement for nodemailer library

--- utils.ts
+++ utils.ts
@@ -1,39 +1,31 @@
-// returns a random number between 1 and 10 inclusive
-export function getRandomNumber(): number {
-  return Math.floor(Math.random() * 10) + 1;
-}
+import nodemailer from 'nodemailer';
 
 /**
- * Sends an email every hour to the specified email address with a random number between 1 and 10.
+ * Sends an email to the specified user every hour with a random number between 1 and 10
  */
-export async function sendHourlyEmail(): Promise<void> {
-  const nodemailer = require('nodemailer');
-  const smtpHost = process.env.SMTP_HOST;
-  const smtpPort = process.env.SMTP_PORT;
-  const smtpSecure = process.env.SMTP_SECURE === 'true';
-  const smtpUser = process.env.SMTP_USER;
-  const smtpPass = process.env.SMTP_PASS;
-  const smtpFrom = process.env.SMTP_FROM;
-  const smtpTo = process.env.SMTP_TO;
-
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: smtpHost,
-    port: Number(smtpPort),
-    secure: smtpSecure, // true for 465, false for other ports
+export function sendHourlyEmail() {
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: parseInt(process.env.SMTP_PORT),
+    secure: process.env.SMTP_SECURE === 'true',
     auth: {
-      user: smtpUser,
-      pass: smtpPass
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
     }
   });
-
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: smtpFrom, // sender address
-    to: smtpTo, // list of receivers
-    subject: 'Hourly Number', // Subject line
-    text: `Your random number is ${getRandomNumber()}.`, // plain text body
+  
+  const message = {
+    from: process.env.SMTP_FROM,
+    to: process.env.SMTP_TO,
+    subject: 'Hourly random number',
+    text: `Here's your hourly random number: ${Math.floor(Math.random() * 10) + 1}`
+  };
+  
+  transporter.sendMail(message, (error) => {
+    if (error) {
+      console.error('Error sending email:', error);
+    } else {
+      console.log('Email sent successfully!');
+    }
   });
-
-  console.log(`Message sent: ${info.messageId}`);
 }
