efea81f290a5629c4418ba38446652d3dc4f5f57
coco-b0t <92187713+coco-b0t@users.noreply.github.com>
2023-04-15 20:08:20 -0500

    Import nodemailer in utils.ts and set up function to send emails every hour with the random number to the user.

--- utils.ts
+++ utils.ts
@@ -1,31 +1,41 @@
 import nodemailer from 'nodemailer';
 
 /**
- * Sends an email to the specified user every hour with a random number between 1 and 10
+ * This function sends an email every hour with a random number between 1 and 10, using the nodemailer package.
  */
-export function sendHourlyEmail() {
-  const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: parseInt(process.env.SMTP_PORT),
-    secure: process.env.SMTP_SECURE === 'true',
-    auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS
+export function sendHourlyEmail(): void {
+    const SMTP_HOST = process.env.SMTP_HOST;
+    const SMTP_PORT = parseInt(process.env.SMTP_PORT as string);
+    const SMTP_SECURE = process.env.SMTP_SECURE === 'true';
+    const SMTP_USER = process.env.SMTP_USER;
+    const SMTP_PASS = process.env.SMTP_PASS;
+    const SMTP_FROM = process.env.SMTP_FROM;
+    const SMTP_TO = process.env.SMTP_TO;
+    
+    const transport = nodemailer.createTransport({
+        host: SMTP_HOST,
+        port: SMTP_PORT,
+        secure: SMTP_SECURE,
+        auth: {
+            user: SMTP_USER,
+            pass: SMTP_PASS
+        }
+    });
+    
+    const randomNum = Math.floor(Math.random() * 10) + 1;
+    
+    const mailOptions = {
+        from: SMTP_FROM,
+        to: SMTP_TO,
+        subject: 'Random Number Hourly Update',
+        text: `Your random number this hour is: ${randomNum}`
     }
-  });
-  
-  const message = {
-    from: process.env.SMTP_FROM,
-    to: process.env.SMTP_TO,
-    subject: 'Hourly random number',
-    text: `Here's your hourly random number: ${Math.floor(Math.random() * 10) + 1}`
-  };
-  
-  transporter.sendMail(message, (error) => {
-    if (error) {
-      console.error('Error sending email:', error);
-    } else {
-      console.log('Email sent successfully!');
-    }
-  });
+    
+    transport.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
 }
