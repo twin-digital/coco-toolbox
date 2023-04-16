e7032ef03fb91e038f4aec081e8404c4a9faa8db
coco-b0t <92187713+coco-b0t@users.noreply.github.com>
2023-04-16 01:23:52 -0500

    Created the test cases for the cosine function in TypeScript

--- utils/cosine.test.ts
+++ utils/cosine.test.ts
@@ -1,19 +1,31 @@
 import { cosine } from './cosine';
 
 describe('cosine', () => {
-  it('should return 1 for 0 radians', () => {
-    expect(cosine(0)).toBe(1);
+  it('should return the correct value for positive input', () => {
+    expect(cosine(Math.PI/3)).toBeCloseTo(0.5);
   });
 
-  it('should return -1 for pi radians', () => {
-    expect(cosine(Math.PI)).toBe(-1);
+  it('should return the correct value for negative input', () => {
+    expect(cosine(-Math.PI/4)).toBeCloseTo(0.707);
   });
 
-  it('should throw an error if the angle is less than -pi', () => {
-    expect(() => cosine(-Math.PI - 0.1)).toThrow('The input value must be between -pi and pi.');
+  it('should return 1 when input angle is 0', () => {
+    expect(cosine(0)).toBeCloseTo(1);
   });
 
-  it('should throw an error if the angle is greater than pi', () => {
-    expect(() => cosine(Math.PI + 0.1)).toThrow('The input value must be between -pi and pi.');
+  it('should throw an error when input angle is less than -pi', () => {
+    expect(() => cosine(-Math.PI - 0.1)).toThrowError('Input angle is outside the standard range: -pi to pi');
+  });
+
+  it('should throw an error when input angle is greater than pi', () => {
+    expect(() => cosine(Math.PI + 0.1)).toThrowError('Input angle is outside the standard range: -pi to pi');
+  });
+
+  it('should return NaN when input is NaN', () => {
+    expect(cosine(NaN)).toBeNaN();
+  });
+
+  it('should return Infinity when input is Infinity', () => {
+    expect(cosine(Infinity)).toBe(Infinity);
   });
 });
