696a650569701770bd38b67dc4bcdf8659b2aa39
coco-b0t <92187713+coco-b0t@users.noreply.github.com>
2023-04-16 01:23:51 -0500

    Created the cosine function in TypeScript

--- utils/cosine.ts
+++ utils/cosine.ts
@@ -1,13 +1,12 @@
-/**
- * Calculates the cosine of the given angle in radians.
- * @param angleInRadians The angle in radians.
- * @returns The cosine value of the angle.
- * @throws An error if the input value is outside the standard range of -pi to pi
+/*
+ * Calculates the cosine of a given angle in radians
+ * @param angleInRadians The angle in radians
+ * @throws Error if input angle is outside the standard range: -pi to pi
+ * @returns The cosine value of the input angle with precision up to 3 decimals
  */
 export function cosine(angleInRadians: number): number {
   if (angleInRadians < -Math.PI || angleInRadians > Math.PI) {
-    throw new Error('The input value must be between -pi and pi.');
+    throw new Error('Input angle is outside the standard range: -pi to pi');
   }
-
-  return Number(Math.cos(angleInRadians).toFixed(3));
+  return parseFloat(Math.cos(angleInRadians).toFixed(3));
 }
